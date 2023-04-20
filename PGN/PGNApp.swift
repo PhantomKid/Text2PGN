04db73a76ed5ee57be24f419926a1efb0744e475
Kai <Kai@example.com>
2023-04-20 09:44:29 +0800

    Initial Commit

---
+++ PGN/PGNApp.swift
@@ -0,0 +1,17 @@
+//
+//  PGNApp.swift
+//  PGN
+//
+//  Created by Kai on 2023/4/20.
+//
+
+import SwiftUI
+
+@main
+struct PGNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
